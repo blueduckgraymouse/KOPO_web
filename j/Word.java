587827515ef04411b030cd95fa27f1262bf4e28f
KOPO_35 <hyunduok@gmail.com>
2022-06-09 11:28:48 +0900

    수정) return boolean 변수로 처리

--- j/Word.java
+++ j/Word.java
@@ -17,12 +17,17 @@ public class Word {
 	
 	// i번째 글자가 모음인지 확인, a,i,u,e,o
 	public boolean isVowel(int i) {
-		String[] vowel = {"a", "i", "u", "e", "o"};
-		for (int j = 0 ; j < vowel.length ; j++) {
-			if (letters.subSequence(i, i+1).equals(vowel[j])) {
-				return true;
+		boolean result = false;
+		String[] vowels = {"a", "i", "u", "e", "o"};
+		//char[] vowels = {'a', 'i', 'u', 'e', 'o'};
+		//	-> charsquence를 인터페이스를 상속받은 클래스는 CharBuffer, Segment, String, StringBuffer, StringBuilder이므로
+		//	   equals ()메서드에서는 false.
+		for (int j = 0 ; j < vowels.length ; j++) {
+			if (letters.substring(i, i +1).equals(vowels[j])) {
+				result = true;		// 중간에 return을 하게 되면 코드가 길어졌을 때 
+				break;				// 코드 이해가 어려워지므로 명시적을 변수로 만들어 마지막에 return하는 것을 추천.
 			}
 		}
-		return false;
+		return result;
 	}
 }
